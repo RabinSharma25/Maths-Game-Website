5e6337936f05d5edfd88153dfe0cf7e272569529
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-01-11 18:29:43 +0530

    updated optimized

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    // $con = mysqli_connect("sql208.liveblog365.com","lblog_33351065","r9ytezdmg9p", "lblog_33351065_game_db");
     $con = mysqli_connect("localhost","root","", "test");
 
     // Check connection
